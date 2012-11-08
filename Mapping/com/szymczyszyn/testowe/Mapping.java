7343feba56e3455a27f3cc27c74d3c66d0d864d9
tomasz <szymczyszyn.tomasz@gmail.com>
2012-11-08 19:49:26 +0000

    dodatkowe metody

--- Mapping/com/szymczyszyn/testowe/Mapping.java
+++ Mapping/com/szymczyszyn/testowe/Mapping.java
@@ -3,16 +3,29 @@ package com.szymczyszyn.testowe;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Mapping {
 
+	String komenda;
+	String klucz;
+	String wartosc;
 	public void init() throws IOException
 	{
 		while(true)
 		{
 			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-			System.out.println(br.readLine());
+			analizujWiersz(br.readLine());
+			System.out.println(komenda+" "+klucz+" "+wartosc);
 		}
 	}
+	public void analizujWiersz(String wiersz)
+	{
+		StringTokenizer st = new StringTokenizer(wiersz);
+		if(st.hasMoreTokens()) komenda=st.nextToken();
+		if(st.hasMoreTokens()) klucz=st.nextToken();
+		if(st.hasMoreTokens()) wartosc=st.nextToken();
+	}
+	
 	
 }
